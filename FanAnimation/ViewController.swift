bb5b429d3dcf002524726aaf9b5824e27e6aeaff
Alaa Dergham <alaa.durgam@gmail.com>
2020-05-10 18:10:17 +0300

    Initial Commit

---
+++ FanAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FanAnimation
+//
+//  Created by Alaa Dergham on 5/10/20.
+//  Copyright © 2020 Alaa Dergham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
